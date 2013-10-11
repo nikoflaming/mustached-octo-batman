02963b7fd24dff5b7193b2d17444fc325626829e
nikoflaming <nikoo.floyd@gmail.com>
2013-10-10 21:56:05 -0300

    new commit

--- README.md
+++
@@ -1,2 +0,0 @@
-mustached-octo-batman
-=====================
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Test1::Application.config.secret_key_base = '46c8fa6fbc046c61ba278ea9dd0e0c204eba6bb2cd09352c772ef79144cc0781b7082a8ca033bea64660ec6536c2d5bb55bd309c9b1b399db17be10dc40c4787'
+Pedaleando::Application.config.secret_token = 'ddf64c2213259baaa252435d73e0ec390bd3782f717f41fb2297403aeaae8575056ada08258f57dc37f22ad7e6bf88cbd57eed447a96d3b191fbced709aca306'
